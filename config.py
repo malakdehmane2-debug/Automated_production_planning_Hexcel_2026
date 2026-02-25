66dc15d6c3cdf727aa0d610a4fd7df54452d3a4e
zohair <zohair@gmail.com>
2026-02-25 15:14:24 +0000

    dev cloud

--- config.py
+++ config.py
@@ -6,8 +6,10 @@ BASEDIR = Path(__file__).parent.absolute()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'votre-cle-secrete-tres-longue'
+
     # Chemin absolu vers la base de données
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + str(BASEDIR / 'database.db')
+    # Permet d'utiliser une URL fournie par l'environnement (ex : VERCEL)
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or ('sqlite:///' + str(BASEDIR / 'database.db'))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     # Configuration pour les uploads d'images
